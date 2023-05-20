3ce07dc0270bea5dc64b99c044932d88f3fcaed7
Mitchell Harrington <ya-hell@hotmail.com>
2023-05-20 12:30:45 +1000

    Inital Code

---
+++ run.py
@@ -0,0 +1,109 @@
+import discord
+from discord.ext import commands
+import datetime
+import asyncio
+import logging
+import sqlite3
+
+TOKEN = 'your_bot_token'
+MAX_HOURS = 5  # The maximum number of hours for the rank
+DATABASE_FILE = 'bot.db'  # SQLite database file name
+
+intents = discord.Intents.default()
+intents.voice_states = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+# Configure logger
+logger = logging.getLogger('discord')
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler(filename='bot.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+# Create SQLite database connection
+conn = sqlite3.connect(DATABASE_FILE)
+cursor = conn.cursor()
+
+# Create table if it doesn't exist
+cursor.execute('''CREATE TABLE IF NOT EXISTS voice_records (
+                    user_id INTEGER PRIMARY KEY,
+                    total_time INTEGER DEFAULT 0
+                )''')
+conn.commit()
+
+@bot.event
+async def on_ready():
+    logger.info(f'Logged in as {bot.user.name}')
+
+@bot.event
+async def on_voice_state_update(member, before, after):
+    if before.channel == after.channel:
+        return  # Ignore if the user didn't change the voice channel
+
+    if after.channel is not None:
+        # User joined a voice channel
+        if not member.bot:
+            await start_timer(member)
+
+    if before.channel is not None:
+        # User left a voice channel
+        if not member.bot:
+            await stop_timer(member)
+
+async def start_timer(member):
+    if member.id not in bot.voice_timers:
+        bot.voice_timers[member.id] = datetime.datetime.now()
+
+async def stop_timer(member):
+    if member.id in bot.voice_timers:
+        start_time = bot.voice_timers.pop(member.id)
+        end_time = datetime.datetime.now()
+        duration = end_time - start_time
+        total_hours = duration.total_seconds() / 3600
+
+        if total_hours >= MAX_HOURS:
+            rank_role = discord.utils.get(member.guild.roles, name='Rank Role')
+            await member.add_roles(rank_role)
+
+        # Update the total time in the database
+        cursor.execute("SELECT total_time FROM voice_records WHERE user_id = ?", (member.id,))
+        row = cursor.fetchone()
+        if row is None:
+            cursor.execute("INSERT INTO voice_records (user_id, total_time) VALUES (?, ?)",
+                           (member.id, duration.total_seconds()))
+        else:
+            total_time = row[0] + duration.total_seconds()
+            cursor.execute("UPDATE voice_records SET total_time = ? WHERE user_id = ?",
+                           (total_time, member.id))
+        conn.commit()
+
+@bot.command()
+async def set_rank_role(ctx, role_name):
+    rank_role = discord.utils.get(ctx.guild.roles, name=role_name)
+    if rank_role:
+        bot.rank_role = rank_role
+        await ctx.send(f"Rank role set as '{role_name}'.")
+    else:
+        await ctx.send(f"Role '{role_name}' not found.")
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        return  # Ignore command not found errors
+
+    # Log the error
+    logger.error(f'Command Error: {error}', exc_info=True)
+
+@bot.event
+async def on_error(event, *args, **kwargs):
+    # Log the error
+    logger.error(f'Unhandled Error: {event}', exc_info=True)
+
+# Initialize the voice_timers dictionary
+bot.voice_timers = {}
+
+bot.run(TOKEN)
+
+# Close the database connection
+conn.close()
